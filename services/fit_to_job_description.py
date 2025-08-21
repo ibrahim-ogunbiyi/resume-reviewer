b2b8612f8e1c72c62922853dbe5ca0f8839f8974
Ibrahim Abayomi Ogunbiyi <Ibrahimabayomi234@gmail.com>
2025-08-21 12:31:39 +0100

    Fit to Job Description

--- services/fit_to_job_description.py
+++ services/fit_to_job_description.py
@@ -1,33 +1,36 @@
+import re
 from lib.llm import llm
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
+from prompts.prompt import JOB_DESCRIPTION_CHECK
+from .schema import JobDescriptionCheckSchema, FileSchema
+from langchain_core.output_parsers import JsonOutputParser
+from parser.parser_factory import get_parser
 
-# llm.invoke(
 
-# ) 
+def fit_to_job_description(file:FileSchema, job_description:str):
 
+    with get_parser(doc_bytes=file.doc_bytes, ext=file.ext) as parser:
+        extracted_text = parser.extract_text()
 
+    parser = JsonOutputParser(pydantic_object=JobDescriptionCheckSchema)
 
-def fit_to_job_description():
-
-    prompt =  ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            
-        ),
-
-        ("human", "{resume}", "{job_description}")
-
-    ]
+    prompt = PromptTemplate(
+        template=JOB_DESCRIPTION_CHECK,
+        input_variables=["user_resume_or_cv", "job_description"],
+        partial_variables={"output_format":parser.get_format_instructions()}
     )
 
     chain =  prompt | llm
 
-    chain.invoke(
+    result = chain.invoke(
 
         {
-            "resume": "None",
-            "job_description": "None"
+            "user_resume_or_cv": extracted_text,
+            "job_description": job_description
         }
-    )      
+    )
+
+    result = re.sub(r'^```json|```', '', result.content.strip(), flags=re.MULTILINE)
+
+    return result
 
