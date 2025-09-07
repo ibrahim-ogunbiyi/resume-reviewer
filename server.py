58c211f4ec0a258b334c64762d31864a62baf20f
Ibrahim Abayomi Ogunbiyi <Ibrahimabayomi234@gmail.com>
2025-09-07 08:14:10 +0100

    Stage Changes

---
+++ server.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from pathlib import Path
+from services.schema import FileSchema
+from services.fit_to_job_description import fit_to_job_description
+
+st.title("Resume Review: Tailor Resume to Job Description")
+
+uploaded_file = st.file_uploader(label="Upload File", type=["pdf", "docx", "doc"])
+job_description = st.text_area(label="Enter or Paste Job Description")
+
+result = None
+if uploaded_file and job_description:
+    doc_bytes = uploaded_file.getvalue()
+    file_extension = Path(uploaded_file.name).suffix
+    file = FileSchema(doc_bytes=doc_bytes, ext=file_extension)
+    result = fit_to_job_description(file=file, job_description=job_description)
+
+if st.button("Process"):
+    with st.spinner("Processing... ⏳"):
+        if result:
+            st.header("📋 Resume Review")
+            for check in result["checks"]:
+                with st.expander(f"🔹 {check['pillar']}"):
+                    st.markdown(f"**🚨 Problem:** {check['problem']}")
+                    st.markdown(f"**✅ Recommendation:** {check['recommendation']}")
--- services/fit_to_job_description.py
+++ services/fit_to_job_description.py
@@ -1,10 +1,15 @@
 import re
+import json
+import torch
+from sentence_transformers import util, SentenceTransformer
+from keybert import KeyBERT
 from lib.llm import llm
 from langchain_core.prompts import PromptTemplate
 from prompts.prompt import JOB_DESCRIPTION_CHECK
 from .schema import JobDescriptionCheckSchema, FileSchema
 from langchain_core.output_parsers import JsonOutputParser
 from parser.parser_factory import get_parser
+
 
 
 def fit_to_job_description(file:FileSchema, job_description:str):
@@ -32,5 +37,38 @@ def fit_to_job_description(file:FileSchema, job_description:str):
 
     result = re.sub(r'^```json|```', '', result.content.strip(), flags=re.MULTILINE)
 
+    result = json.loads(result)
     return result
+
+
+
+def ats_checker(job_description:str, resume_text:str):
+
+    keybert_model = KeyBERT("all-MiniLM-L6-v2")
+    embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    job_description_phrases = keybert_model.extract_keywords(docs=job_description, keyphrase_ngram_range=(1, 3))
+
+    job_description_embeddings = embedding_model.encode(job_description_phrases)
+    
+    resume_phrases = keybert_model.extract_keywords(docs=resume_text, keyphrase_ngram_range=(1, 3))
+
+    all_similarities = []
+
+    for resume_phrase in resume_phrases:
+
+        phrase_embedding = embedding_model.encode(resume_phrase)
+        similarities = util.cos_sim(phrase_embedding, job_description_embeddings)
+
+        highest_similary = torch.max(similarities)
+
+        all_similarities.append(highest_similary)
+
+    return torch.mean(all_similarities)
+
+
+
+
+
+
 
