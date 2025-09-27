37c0167ceadfca98857e4f828371bfaec5bd01e5
Ibrahim Ogunbiyi <geek@Ibrahims-MacBook-Pro.local>
2025-09-27 15:40:49 +0100

    Add streamlit secrets option

--- core/config.py
+++ core/config.py
@@ -1,10 +1,14 @@
+import streamlit as st
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    LLM_API_KEY: str = Field(validation_alias="groq_api_key")
+    try:
+        model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+        LLM_API_KEY: str = Field(validation_alias="groq_api_key")
+    except Exception:
+        LLM_API_KEY = st.secrets["GROQ_API_KEY"]
     MODEL_NAME: str = "openai/gpt-oss-20b"
     EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L12-v2"
 
