f2432a35993b9f07b7aabbb4440f7684a60b219d
Ibrahim Ogunbiyi <geek@Ibrahims-MacBook-Pro.local>
2025-09-27 15:33:41 +0100

    Convert Embedding and KeyBERT to FastAPI endpoint

--- services/fit_to_job_description.py
+++ services/fit_to_job_description.py
@@ -3,6 +3,7 @@ import re
 from parser.parser_factory import get_parser
 
 import numpy as np
+import requests
 import torch
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
@@ -14,47 +15,62 @@ from prompts.prompt import JOB_DESCRIPTION_CHECK
 from .schema import FileSchema, JobDescriptionCheckSchema
 
 
-def extract_phrases(text: str, ngram_range: tuple, top_n: int) -> list[str]:
-    keybert_model = ModelClass().keybert_model
+def extract_phrases(text:list[str] | str) -> list:
+    
+    url = "https://ibrahim-geek-encode-and-extract-phrases.hf.space/extract_keyword_phrases"
 
-    key_phrases = keybert_model.extract_keywords(
-        docs=text, keyphrase_ngram_range=ngram_range, top_n=top_n
-    )
+    response = requests.post(url=url, json={"text":text})
 
-    result = [phrase[0] for phrase in key_phrases]
+    return response.json()
 
-    return result
+def encode_text(text:list[str] | str) -> list:
+    
+    url = "https://ibrahim-geek-encode-and-extract-phrases.hf.space/get_encoding"
+
+    response = requests.post(url=url, json={"text":text})
+
+    return response.json()
 
 
-def ats_checker(job_description: str, resume_text: str) -> tuple[int, dict]:
-    # initialize model
-    encoding_model = ModelClass().encoding_model
+def ats_checker(job_description: str, resume_text: str) -> tuple[dict, dict]:
 
     # extract keyword phrases
-    job_description_phrases = extract_phrases(text=job_description, ngram_range=(1, 3), top_n=-1)
+    job_description_phrases = extract_phrases(text=job_description)
+
+    resume_phrases = extract_phrases(text=resume_text)
 
     # get embeddings.
-    job_description_embeddings = encoding_model.encode(job_description_phrases)
+    job_description_embeddings = torch.Tensor(encode_text(job_description_phrases))
 
-    resume_phrases = extract_phrases(text=resume_text, ngram_range=(1, 3), top_n=-1)
+    phrases_embeddings = torch.Tensor(encode_text(resume_phrases))
 
     all_similarities = []
 
-    for resume_phrase in resume_phrases:
-        phrase_embedding = encoding_model.encode(resume_phrase)
+    matched_phrases = []
+
+    for i, phrase_embedding in enumerate(phrases_embeddings):
         similarities = util.cos_sim(phrase_embedding, job_description_embeddings)
 
         highest_similary = torch.max(similarities)
 
         all_similarities.append(highest_similary)
 
-    phrases = {"job_description_phrases": job_description_phrases, "resume_phrases": resume_phrases}
+        if highest_similary >=0.6:
+            matched_phrases.append(resume_phrases[i])
 
-    return np.mean(all_similarities), phrases
+    phrases = {
+        "job_description_phrases": job_description_phrases,
+        "resume_phrases": resume_phrases, "matched_phrases": matched_phrases
+    }
+    
+    scores = {"mean": (np.mean(all_similarities)*100), "std":(np.std(all_similarities)*100)}
+
+    return scores, phrases
 
 
-def fit_to_job_description(file: FileSchema, job_description: str) -> tuple[dict, tuple[int, dict]]:
+def fit_to_job_description(file: FileSchema, job_description: str) -> tuple[dict, tuple[dict, dict]]:
     with get_parser(doc_bytes=file.doc_bytes, ext=file.ext) as parser:
+
         extracted_text = parser.extract_text()
 
     parser = JsonOutputParser(pydantic_object=JobDescriptionCheckSchema)
@@ -76,4 +92,5 @@ def fit_to_job_description(file: FileSchema, job_description: str) -> tuple[dict
     result = json.loads(result)
 
     ats_checker_result = ats_checker(job_description=job_description, resume_text=extracted_text)
+    
     return result, ats_checker_result
