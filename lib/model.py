20f08911f2e8eb8b6b6268c9cce66212cc2c4b4c
Ibrahim <ibrahimabayomi234@gmail.com>
2025-10-22 12:15:00 +0100

    update model.py

--- lib/model.py
+++ lib/model.py
@@ -1,35 +1,10 @@
-from keybert import KeyBERT
 from langchain_groq import ChatGroq
-from sentence_transformers import SentenceTransformer
 
 from core.config import settings
 
 
 class ModelClass:
-
-    keybert_model = None
-    encoding_model = None
     llm = None
-
-    @classmethod
-    def get_keybert_model(cls) -> None:
-
-        if cls.keybert_model is None:
-
-            cls.keybert_model = KeyBERT(settings.EMBEDDING_MODEL)
-
-            # warmup model
-            _ = cls.keybert_model.extract_keywords("Dummy testing to warmup model")
-    
-    @classmethod
-    def get_encoding_model(cls) -> None:
-
-        if cls.encoding_model is None:
-
-            cls.encoding_model = SentenceTransformer(settings.EMBEDDING_MODEL)
-
-            # warmup encoding model
-            _ = cls.encoding_model.encode("Dummy testing to warm up model")
 
     @classmethod
     def get_llm(cls) -> None:
